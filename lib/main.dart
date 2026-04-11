3aafbef480132966cefd4e1aa722f28ef604ab6a
Kadir Güneş <155745941+kadirgunes0@users.noreply.github.com>
2026-04-11 13:47:52 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // flutterfire configure ile oluşan dosya
-import 'screens/main_screen/login.dart'; // Senin login dosyan
+import 'firebase_options.dart';
+import 'screens/main_screen/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase'i başlatan kritik satır
+  //Firebasei başlat
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
